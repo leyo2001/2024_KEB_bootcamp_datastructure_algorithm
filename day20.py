b5e2ed5750e09b4d1c9a5abfd3be9c499176f7f4
leyo2001 <leyo2001@naver.com>
2024-02-08 15:43:51 +0900

    v0.7 code0306 renewal

--- day20.py
+++ day20.py
@@ -1,10 +1,10 @@
 ## 함수 선언 부분 ## 
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1     # 최고차항 숫자 = 배열길이-1
+def print_poly(t_x, f_x) -> str:
     poly_expression = "P(x) = "
 
     for i in range(len(fx)):
-        coefficient = f_x[i]  # 계수
+        coefficient = f_x[i]
+        term = t_x[i]
 
         if (coefficient > 0):
             if(i == 0):
@@ -16,7 +16,6 @@ def print_poly(f_x) -> str:
             pass
 
         else:
-            term -= 1
             continue
 
         if (term > 0):
@@ -26,32 +25,30 @@ def print_poly(f_x) -> str:
             poly_expression = poly_expression + f'{coefficient}'
 
 
-
-        term -= 1
-
     return poly_expression
 
 
-def calculation_Poly(x_value, f_x) -> int:
+def calculation_Poly(x_value, t_x, f_x) -> int:
     return_Value = 0
-    term = len(f_x) - 1  # 최고차항 숫자 = 배열길이-1
 
     for i in range(len(fx)):
         coefficient = f_x[i]  # 계수
+        term = t_x[i]
         return_Value += coefficient * x_value ** term
-        term -= 1
+
 
     return return_Value
 
 
 ## 전역 변수 선언 부분 ## 
 fx = [7, -4, 0, 5]  # = 7x^3 -4x^2 +0x^1 +5x^0
+tx = [3, 2,  1,  0] # 지수
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
 
-    print(print_poly(fx))
-    print(calculation_Poly(int(input("X 값-->")), fx))
+    print(print_poly(tx ,fx))
+    print(calculation_Poly(int(input("X 값-->")), tx, fx))
 
 
 
