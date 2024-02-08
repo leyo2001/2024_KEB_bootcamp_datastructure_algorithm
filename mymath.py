31f1a5161365a3b81e4eb62a46c6be744f3ca4ad
leyo2001 <leyo2001@naver.com>
2024-02-08 11:27:10 +0900

    v0.4 combination + ãdecorator

--- mymath.py
+++ mymath.py
@@ -1,4 +1,20 @@
 import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"소요시간 : {end - start:.20f}")
+        return result
+    return wrapper
+
+
+
+
+
+
 def factorial(number) -> int:
     '''
     factorial by recursion
@@ -11,16 +27,19 @@ def factorial(number) -> int:
         return number * factorial(number - 1)
 
 
-def nCr(n, r) -> int:
+@timer
+def nCr(n, r) -> int: #SRP 준수
     '''
     조합 함수
     :param n:
     :param r:
     :return:
     '''
-    start = time.time()
+
     numerator = factorial(n)
     denominator = factorial(n-r) * factorial(r)
-    end = time.time()
-    print(f"소요시간 : {end - start:.20f}")
+
     return int(numerator / denominator)
+
+
+
