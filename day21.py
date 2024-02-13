8d0b4f64e44ecdfca2c6b9c42281831b51eed6b5
leyo2001 <leyo2001@naver.com>
2024-02-13 17:05:58 +0900

    v1.2 binary tree order

--- day21.py
+++ day21.py
@@ -1,21 +1,65 @@
-def check_bracket(expr: str) -> bool:
-    """
-    check bracket in expression.
-    :param expr: str
-    :return: bool
-    """
-    stack = list()
-    table = {')': '(', ']': '[', '}': '{', '>': '<'}
-    for char in expr:
-        if char not in table:
-            stack.append(char)
-        elif not stack or table[char] != stack.pop():
-             return False
-        else:
-            pass
-    return len(stack) == 0
+class TreeNode:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+
+node1 = TreeNode()
+node1.data = "화사"
+
+node2 = TreeNode()
+node2.data = "솔라"
+node1.left = node2
+
+node3 = TreeNode()
+node3.data = "문별"
+node1.right = node3
+
+node4 = TreeNode()
+node4.data = "휘인"
+node2.left = node4
+
+node5 = TreeNode()
+node5.data = "쯔위"
+node2.right = node5
+
+node6 = TreeNode()
+node6.data = "선미"
+node3.left = node6
+
+node7 = TreeNode()
+node7.data = "다현"
+node4.right = node7
+
+node8 = TreeNode()
+node8.data = "선미"
+node6.right = node8
 
 
-if __name__ == "__main__":
-    expression = input("Input expression : ")
-    print(check_bracket(expression))
+def preorder(node):
+    if node == None:
+        return
+    print(node.data, end = " ")
+    preorder(node.left)
+    preorder(node.right)
+
+def inorder(node):
+    if node == None:
+        return
+    inorder(node.left)
+    print(node.data, end = " ")
+    inorder(node.right)
+
+def postorder(node):
+    if node == None:
+        return
+    postorder(node.left)
+    postorder(node.right)
+    print(node.data, end = " ")
+
+
+preorder(node1)
+print()
+inorder(node1)
+print()
+postorder(node1)
