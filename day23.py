1bc9b2dd00e0ca33519bf951e3817c0f9973a561
leyo2001 <leyo2001@naver.com>
2024-02-15 13:40:28 +0900

    v2.3 select sort

--- day23.py
+++ day23.py
@@ -1,34 +1,20 @@
-import tkinter as tk
+arr = [1,10,4,5,9,8,7]
 
 
-def fibo(n):
-    l = [0, 1]
-
-    for _ in range(n - 1):
-        l.append(l[len(l) - 1] + l[len(l) - 2])
-    return l[n]
-
-def process_fibonacci():
-    number = int(en_input_number.get())
-    lbl_display_fibonacci_result.config(text = f'fibonacci{[number]} = {fibo(number)}')
 
 
-if __name__ == '__main__':
 
-    w = tk.Tk() # create window object
-    w.title("Fibonacci")
-    w.geometry("250x150")
+for i in range(len(arr)-1):
+    for j in range(i+1,len(arr)):
+        if arr[j] < arr[i]:
+            arr[i], arr[j] = arr[j], arr[i]
 
-    # create widget
-    lbl_display_fibonacci_result = tk.Label(w, text='Fibonacci by memorization')
-    en_input_number = tk.Entry(w)
-    btn_click = tk.Button(w, text = 'Click',command = process_fibonacci) # bind function
+print(arr)
 
-    # layout
-    lbl_display_fibonacci_result.pack()
-    en_input_number.pack(fill='x')
-    btn_click.pack(fill='x')
 
-    en_input_number.focus()
-    w.mainloop()
+
+
+
+
+
 
