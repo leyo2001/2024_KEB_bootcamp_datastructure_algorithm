3f1dec9243e16e8a6439c8c9c8d4ad9c3a90c5e2
leyo2001 <leyo2001@naver.com>
2024-02-15 11:20:36 +0900

    v2.2 fibonacci after gui

--- day23.py
+++ day23.py
@@ -8,19 +8,27 @@ def fibo(n):
         l.append(l[len(l) - 1] + l[len(l) - 2])
     return l[n]
 
-w = tk.Tk() # create window object
-w.title("Fibonacci")
-w.geometry("250x150")
+def process_fibonacci():
+    number = int(en_input_number.get())
+    lbl_display_fibonacci_result.config(text = f'fibonacci{[number]} = {fibo(number)}')
 
-# create widget
-lbl_display_fibonacci_result = tk.Label(w, text='Fibonacci by memorization')
-en_input_number = tk.Entry(w)
-btn_click = tk.Button(w, text = 'Click')
 
-# layout
-lbl_display_fibonacci_result.pack()
-en_input_number.pack(fill='x')
-btn_click.pack(fill='x')
+if __name__ == '__main__':
 
-w.mainloop()
+    w = tk.Tk() # create window object
+    w.title("Fibonacci")
+    w.geometry("250x150")
+
+    # create widget
+    lbl_display_fibonacci_result = tk.Label(w, text='Fibonacci by memorization')
+    en_input_number = tk.Entry(w)
+    btn_click = tk.Button(w, text = 'Click',command = process_fibonacci) # bind function
+
+    # layout
+    lbl_display_fibonacci_result.pack()
+    en_input_number.pack(fill='x')
+    btn_click.pack(fill='x')
+
+    en_input_number.focus()
+    w.mainloop()
 
